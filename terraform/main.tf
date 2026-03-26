3c52d243d77b6ee13df59ceaa8de8a54da7c8a19
Madupa Dilshan <madupadilshan111@gmail.com>
2026-03-26 15:55:25 +0530

    Add DevSecOps CI Pipeline with SAST and Trivy

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu" {
 # 3. Security Group (ආරක්ෂක Firewall එක) සෑදීම
 resource "aws_security_group" "mern_sg" {
   name        = "mern-devops-sg"
-  description = "Allow Web and SSH traffic"
+  description = "Allow Web, SSH and Jenkins traffic"
 
   # අපිට Server එක ඇතුළට යන්න අවසර දීම (SSH - Port 22)
   ingress {
@@ -49,6 +49,14 @@ resource "aws_security_group" "mern_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Jenkins වෙත පිවිසීම සඳහා අලුතින් විවෘත කළ Port එක (Port 8080)
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Server එකෙන් පිටතට යන දත්ත වලට අවසර දීම
   egress {
     from_port   = 0
@@ -70,4 +78,31 @@ resource "aws_instance" "mern_server" {
   tags = {
     Name = "MERN-DevSecOps-Server"
   }
+
+  # ස්වයංක්‍රීයව මෘදුකාංග Install කිරීමේ කේතය (DevSecOps Automation - User Data)
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+
+              # 1. Java ස්ථාපනය (Jenkins සඳහා)
+              sudo apt install fontconfig openjdk-17-jre -y
+
+              # 2. Jenkins ස්ථාපනය
+              sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+              echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+              sudo apt-get update -y
+              sudo apt-get install jenkins -y
+              sudo systemctl start jenkins
+              sudo systemctl enable jenkins
+
+              # 3. Docker ස්ථාපනය (අපේ Images ධාවනය කිරීමට)
+              sudo apt install docker.io -y
+              sudo systemctl start docker
+              sudo systemctl enable docker
+
+              # Jenkins සහ සාමාන්‍ය පරිශීලකයාට Docker භාවිතා කිරීමට අවසර දීම
+              sudo usermod -aG docker ubuntu
+              sudo usermod -aG docker jenkins
+              sudo systemctl restart docker
+              EOF
 }
