6027a4c5252c2b193c96bdc056c581b9474b6a27
Madupa Dilshan <madupadilshan111@gmail.com>
2026-03-31 13:14:16 +0530

    Added DevSecOps CI/CD Pipeline

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Basic Vite config for React.
 export default defineConfig({
   plugins: [react()],
 })
